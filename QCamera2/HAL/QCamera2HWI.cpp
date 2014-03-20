AgentFabulous/android_oreo_bacon

Camera: fix memory leak in camera hal
Considerable memory leak was observered during switching camera.
The gCamCapability should be only initialized once.

Change-Id: I873910eb89a73bde1a80bdefc2294dfa129cc5ce

QCamera2/HAL/QCamera2HWI.cpp
@@ -1075,8 +1075,8 @@ int QCamera2HardwareInterface::openCamera()
         ALOGE("camera_open failed.");
         return UNKNOWN_ERROR;
     }
-
-    initCapabilities(mCameraId,mCameraHandle);
+    if (NULL == gCamCapability[mCameraId])
+        initCapabilities(mCameraId,mCameraHandle);
 
     mCameraHandle->ops->register_event_notify(mCameraHandle->camera_handle,
                                               camEvtHandle,

sonyxperiadev/camera

QCamera2/HAL/QCamera2HWI.cpp
@@ -1088,8 +1088,8 @@ int QCamera2HardwareInterface::openCamera()
         ALOGE("camera_open failed.");
         return UNKNOWN_ERROR;
     }
-
-    initCapabilities(mCameraId,mCameraHandle);
+    if (NULL == gCamCaps[mCameraId])
+        initCapabilities(mCameraId,mCameraHandle);
 
     mCameraHandle->ops->register_event_notify(mCameraHandle->camera_handle,
                                               camEvtHandle,