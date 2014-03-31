AgentFabulous/android_oreo_bacon

QCamera2: Start bracketing for non zsl mode
- Start bracketing for non zsl mode

Change-Id: I6ed63dc088d69248803788d1ef7ce13004a2381b

QCamera2/HAL/QCamera2HWI.h
@@ -397,7 +397,7 @@ private:
     int32_t configureBracketing();
     int32_t configureAFBracketing(bool enable = true);
     int32_t configureFlashBracketing();
-    int32_t startZslBracketing(QCameraPicChannel *pZSLchannel);
+    int32_t startBracketing(QCameraPicChannel *pZSLchannel);
     int32_t configureOptiZoom();
     inline void setOutputImageCount(uint32_t aCount) {mOutputCount = aCount;}
     inline uint32_t getOutputImageCount() {return mOutputCount;}

sonyxperiadev/camera

QCamera2/HAL/QCamera2HWI.h
@@ -389,7 +389,7 @@ private:
     int32_t configureBracketing();
     int32_t configureAFBracketing();
     int32_t configureFlashBracketing();
-    int32_t startZslBracketing(QCameraPicChannel *pZSLchannel);
+    int32_t startBracketing(QCameraPicChannel *pChannel);
     int32_t configureOptiZoom();
 
     static void copyList(cam_dimension_t* src_list,