Evervolv/android_hardware_qcom_camera

Camera: Add metadata support for DNG tags
Added metadata support for
- ProfileTone Curve

Change-Id: I42e82221df93f2aee1de5280a268c6d6d28ff664
Signed-off-by: Iliyan Malchev <malchev@google.com>

Conflicts:
	QCamera2/stack/common/cam_intf.h
	QCamera2/stack/common/cam_types.h

QCamera2/HAL3/QCamera3HWI.cpp
@@ -2191,6 +2191,7 @@ QCamera3HardwareInterface::translateFromHalMetadata(
                                 tonemap->tonemap_points_cnt * 2);
              break;
           }
+
           case CAM_INTF_META_COLOR_CORRECT_GAINS:{
              cam_color_correct_gains_t *colorCorrectionGains = (cam_color_correct_gains_t*)
              POINTER_OF(CAM_INTF_META_COLOR_CORRECT_GAINS, metadata);
@@ -2204,6 +2205,17 @@ QCamera3HardwareInterface::translateFromHalMetadata(
                        (camera_metadata_rational_t*)colorCorrectionMatrix->transform_matrix, 3*3);
               break;
           }
+
+          /* DNG file realted metadata */
+          case CAM_INTF_META_PROFILE_TONE_CURVE: {
+             cam_profile_tone_curve *toneCurve = (cam_profile_tone_curve *)
+             POINTER_OF(CAM_INTF_META_PROFILE_TONE_CURVE, metadata);
+             camMetadata.update(ANDROID_SENSOR_PROFILE_TONE_CURVE,
+                                (float*)toneCurve->curve.tonemap_points,
+                                toneCurve->tonemap_points_cnt * 2);
+             break;
+          }
+
           case CAM_INTF_META_PRED_COLOR_CORRECT_GAINS:{
              cam_color_correct_gains_t *predColorCorrectionGains = (cam_color_correct_gains_t*)
              POINTER_OF(CAM_INTF_META_PRED_COLOR_CORRECT_GAINS, metadata);

sonyxperiadev/camera

QCamera2/HAL3/QCamera3HWI.cpp
@@ -2195,6 +2195,13 @@ QCamera3HardwareInterface::translateFromHalMetadata(
         camMetadata.update(ANDROID_COLOR_CORRECTION_TRANSFORM,
             (camera_metadata_rational_t*)colorCorrectionMatrix->transform_matrix, 3*3);
     }
+    if (IS_META_AVAILABLE(CAM_INTF_META_PROFILE_TONE_CURVE, metadata)) {
+        cam_profile_tone_curve *toneCurve = (cam_profile_tone_curve *)
+                POINTER_OF_META(CAM_INTF_META_PROFILE_TONE_CURVE, metadata);
+        camMetadata.update(ANDROID_SENSOR_PROFILE_TONE_CURVE,
+                (float*)toneCurve->curve.tonemap_points,
+                toneCurve->tonemap_points_cnt * 2);
+    }
     if (IS_META_AVAILABLE(CAM_INTF_META_PRED_COLOR_CORRECT_GAINS, metadata)){
         cam_color_correct_gains_t *predColorCorrectionGains = (cam_color_correct_gains_t*)
             POINTER_OF_META(CAM_INTF_META_PRED_COLOR_CORRECT_GAINS, metadata);