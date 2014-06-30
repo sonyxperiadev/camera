b4971e4fd500d87fcac1573091f51c311f20308d
Emilian Peev <epeevs@codeaurora.org>
2014-06-30 15:13:16 +0300

    QCamera2: Flush pending preview callbacks
     During internal preview restarts the notifier
     thread could be blocked in CameraService and
     have pending preview callback notifications.
     Once the thread is unblocked the pending callbacks
     could be invalid and need to be flushed.
     A mechanism is added for flushing pending preview
     callback notifications. This will be done on
     every preview stop.
    
    Change-Id: I2cddfed205d324ec4d386562eaf1f865fba86bd9
    CRs-Fixed: 686850

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -2051,6 +2051,7 @@ int QCamera2HardwareInterface::stopPreview()
     stopChannel(QCAMERA_CH_TYPE_ZSL);
     stopChannel(QCAMERA_CH_TYPE_PREVIEW);
 
+    m_cbNotifier.flushPreviewNotifications();
     // delete all channels from preparePreview
     unpreparePreview();
 
