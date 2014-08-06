OnePlusOSS/android_hardware_qcom_camera

Camera: Remove Bayer specific Exif entries.
Avoid populating exif entries which are specific to Bayer type
of sensor.

Change-Id: Iaacb66efacb4653c25a8e25750707f283923c87b

QCamera2/HAL/QCamera2HWI.cpp
@@ -1030,6 +1030,7 @@ QCamera2HardwareInterface::QCamera2HardwareInterface(int cameraId)
     pthread_cond_init(&m_int_cond, NULL);
 
     memset(m_channels, 0, sizeof(m_channels));
+    memset(&mExifParams, 0, sizeof(mm_jpeg_exif_params_t));
 
 #ifdef HAS_MULTIMEDIA_HINTS
     if (hw_get_module(POWER_HARDWARE_MODULE_ID, (const hw_module_t **)&m_pPowerModule)) {
@@ -6563,6 +6564,22 @@ void QCamera2HardwareInterface::getLogLevel()
 
     ALOGI("%s gCamHalLogLevel=%d",__func__, gCamHalLogLevel);
     return;
+}
+
+/*===========================================================================
+ * FUNCTION   : getSensorType
+ *
+ * DESCRIPTION: Returns the type of sensor being used whether YUV or Bayer
+ *
+ * PARAMETERS :
+ *   None
+ *
+ * RETURN     : Type of sensor - bayer or YUV
+ *
+ *==========================================================================*/
+cam_sensor_t QCamera2HardwareInterface::getSensorType()
+{
+    return gCamCapability[mCameraId]->sensor_type.sens_type;
 }
 
 }; // namespace qcamera

sonyxperiadev/camera

QCamera2/HAL/QCamera2HWI.cpp
@@ -1109,6 +1109,7 @@ QCamera2HardwareInterface::QCamera2HardwareInterface(int cameraId)
     pthread_mutex_init(&m_parm_lock, NULL);
 
     memset(m_channels, 0, sizeof(m_channels));
+    memset(&mExifParams, 0, sizeof(mm_jpeg_exif_params_t));
 
 #ifdef HAS_MULTIMEDIA_HINTS
     if (hw_get_module(POWER_HARDWARE_MODULE_ID, (const hw_module_t **)&m_pPowerModule)) {
@@ -6487,6 +6488,22 @@ void QCamera2HardwareInterface::getLogLevel()
     gCamHalLogLevel = atoi(prop);
 
     return;
+}
+
+/*===========================================================================
+ * FUNCTION   : getSensorType
+ *
+ * DESCRIPTION: Returns the type of sensor being used whether YUV or Bayer
+ *
+ * PARAMETERS :
+ *   None
+ *
+ * RETURN     : Type of sensor - bayer or YUV
+ *
+ *==========================================================================*/
+cam_sensor_t QCamera2HardwareInterface::getSensorType()
+{
+    return gCamCaps[mCameraId]->sensor_type.sens_type;
 }
 
 }; // namespace qcamera