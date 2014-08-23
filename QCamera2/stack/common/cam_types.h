4639d5e4a7832ad2e6341d884a8ea5e4a2ddf248
Ranjith Kagathi Ananda <ranjith@codeaurora.org>
2014-08-23 14:54:27 -0700

    QCamera2/stack: Define cam_opaque_raw_format_t
    Define cam_opaque_raw_format_t
    
    Change-Id: I5e7f65a88b1a18f16de7dc78a988996dc2c448fc

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -1760,4 +1760,9 @@ typedef enum {
     CAM_AWB_INVALID_ALL_LIGHT,
 } cam_illuminat_t;
 
+typedef enum {
+    LEGACY_RAW,
+    MIPI_RAW,
+} cam_opaque_raw_format_t;
+
 #endif /* __QCAMERA_TYPES_H__ */
