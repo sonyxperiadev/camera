aafdc30c047e4b45b4b5c3a6c3ce866fa097e102
Emilian Peev <epeevs@codeaurora.org>
2014-09-02 09:51:25 -0700

    QCamera2: Fix a possible timing issue during cancelPicture
     Both cancel picture and capture channel callback
     can wait on the reprocess channel to finish at the
     same time. In this case the condition needs broadcast
     so that both threads can resume.
    
    Change-Id: Ic9ab4dd7ed6485cc81a478547c9b3ca126987597

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -7088,7 +7088,7 @@ void *QCamera2HardwareInterface::defferedWorkRoutine(void *obj)
                             Mutex::Autolock l(pme->mDeffLock);
                             pme->mDeffOngoingJobs[dw->id] = false;
                             delete dw;
-                            pme->mDeffCond.signal();
+                            pme->mDeffCond.broadcast();
                         }
                     }
                     break;
@@ -7165,7 +7165,6 @@ int32_t QCamera2HardwareInterface::waitDefferedWork(int32_t &job_id)
         mDeffCond.wait(mDeffLock);
     }
 
-    job_id = MAX_ONGOING_JOBS;
     return NO_ERROR;
 }
 
