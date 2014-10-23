RonGokhale/android_hardware_qcom_camera

QCamera2/stack: Define clear_metadata_buffer to replace memset(meta)
metadata_buffer_t is about 1 MB in size and keeps growing as more
member structures are added. At several places in HAL and backend/MCT,
memset is done on this buffer both during init and per-frame, wasting
several CPU cycles. clear_metadata_buffer() function instead sets only the
is_xxx_valid flags inside meta buffer to 0 requiring only 1/5000-th of
original CPU cycles required for memset(metabuffer) operation

Change-Id: I97aba0fe8b6f10ebe642e5f5b756c98c2a401fe8

QCamera2/stack/common/cam_intf.h
@@ -640,6 +640,8 @@ typedef parm_data_t metadata_data_t;
 
 /****************************DO NOT MODIFY BELOW THIS LINE!!!!*********************/
 
+/* Update clear_metadata_buffer() function when a new is_xxx_valid is added to
+ * this structure */
 typedef struct {
     union{
         /* Hash table of 'is valid' flags */
@@ -701,6 +703,19 @@ void *get_pointer_of(cam_intf_parm_type_t meta_id,
         const metadata_buffer_t* metadata);
 
 uint32_t get_size_of(cam_intf_parm_type_t param_id);
+
+/* Update this inline function when a new is_xxx_valid is added to
+ * metadata_buffer_t */
+static inline void clear_metadata_buffer(metadata_buffer_t *meta)
+{
+    memset(meta->is_valid, 0, CAM_INTF_PARM_MAX);
+    meta->is_tuning_params_valid = 0;
+    meta->is_mobicat_ae_params_valid = 0;
+    meta->is_mobicat_awb_params_valid = 0;
+    meta->is_mobicat_af_params_valid = 0;
+    meta->is_mobicat_asd_params_valid = 0;
+    meta->is_mobicat_stats_params_valid = 0;
+}
 
 #ifdef  __cplusplus
 }

sonyxperiadev/camera

QCamera2/stack/common/cam_intf.h
@@ -657,8 +657,8 @@ typedef struct {
 
 typedef parm_data_t metadata_data_t;
 
-/****************************DO NOT MODIFY BELOW THIS LINE!!!!*********************/
-
+/* Update clear_metadata_buffer() function when a new is_xxx_valid is added to
+ * or removed from this structure */
 typedef struct {
     union{
         /* Hash table of 'is valid' flags */
@@ -671,7 +671,6 @@ typedef struct {
     /*Tuning Data */
     uint8_t is_tuning_params_valid;
     tuning_params_t tuning_params;
-
 
     /* Mobicat Params */
     uint8_t is_mobicat_aec_params_valid;
@@ -704,6 +703,20 @@ void *get_pointer_of(cam_intf_parm_type_t meta_id,
         const metadata_buffer_t* metadata);
 
 uint32_t get_size_of(cam_intf_parm_type_t param_id);
+
+/* Update this inline function when a new is_xxx_valid is added to
+ * or removed from metadata_buffer_t */
+static inline void clear_metadata_buffer(metadata_buffer_t *meta)
+{
+    memset(meta->is_valid, 0, CAM_INTF_PARM_MAX);
+    meta->is_tuning_params_valid = 0;
+    meta->is_mobicat_aec_params_valid = 0;
+    meta->is_statsdebug_ae_params_valid = 0;
+    meta->is_statsdebug_awb_params_valid = 0;
+    meta->is_statsdebug_af_params_valid = 0;
+    meta->is_statsdebug_asd_params_valid = 0;
+    meta->is_statsdebug_stats_params_valid = 0;
+}
 
 #ifdef  __cplusplus
 }