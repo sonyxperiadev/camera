RonGokhale/android_hardware_qcom_camera

HAL : Added check for initCapabilities
Change-Id: Ic30084ebb9dafe77daaf0c96d48bf34fd2a05e35

QCamera2/HAL/QCamera2HWI.cpp
@@ -1156,8 +1156,14 @@ int QCamera2HardwareInterface::openCamera()
         ALOGE("camera_open failed.");
         return UNKNOWN_ERROR;
     }
-    if (NULL == gCamCapability[mCameraId])
-        initCapabilities(mCameraId,mCameraHandle);
+    if (NULL == gCamCapability[mCameraId]) {
+        if(NO_ERROR != initCapabilities(mCameraId,mCameraHandle)) {
+            ALOGE("initCapabilities failed.");
+            mCameraHandle->ops->close_camera(mCameraHandle->camera_handle);
+            mCameraHandle = NULL;
+            return UNKNOWN_ERROR;
+        }
+    }
 
     mCameraHandle->ops->register_event_notify(mCameraHandle->camera_handle,
                                               camEvtHandle,

sonyxperiadev/camera

QCamera2/HAL/QCamera2HWI.cpp
@@ -1232,8 +1232,14 @@ int QCamera2HardwareInterface::openCamera()
         ALOGE("camera_open failed.");
         return UNKNOWN_ERROR;
     }
-    if (NULL == gCamCaps[mCameraId])
-        initCapabilities(mCameraId,mCameraHandle);
+    if (NULL == gCamCaps[mCameraId]) {
+        if(NO_ERROR != initCapabilities(mCameraId,mCameraHandle)) {
+            ALOGE("initCapabilities failed.");
+            mCameraHandle->ops->close_camera(mCameraHandle->camera_handle);
+            mCameraHandle = NULL;
+            return UNKNOWN_ERROR;
+        }
+    }
 
     mCameraHandle->ops->register_event_notify(mCameraHandle->camera_handle,
                                               camEvtHandle,