16305ff03e15d584461ec9ce0704d2d44ee229e1
Byunghun Jeon <bjeon@codeaurora.org>
2015-04-14 15:47:40 -0700

    add YUV stride support and remove the commented code
    Adding YUV stride support for beautification
    
    Change-Id: I8ed4ae97e7f678fae6284ca8a9f5f1ded654cee1

--- QCamera2/HAL/QCamera2HWI.h
+++ QCamera2/HAL/QCamera2HWI.h
@@ -705,12 +705,9 @@ private:
    //ts add for makeup
 #ifdef TARGET_TS_MAKEUP
     TSRect mFaceRect;
-    unsigned char *mMakeUpBuf;
-    int yuvDataRelocate(uint8_t* pSrcBuffer,uint8_t* pDstBuffer,cam_frame_len_offset_t offset);
-    int yuvDataRecover(uint8_t* pSrcBuffer,uint8_t* pDstBuffer,cam_frame_len_offset_t offset);
     bool TsMakeupProcess_Preview(mm_camera_buf_def_t *pFrame,QCameraStream * pStream);
     bool TsMakeupProcess_Snapshot(mm_camera_buf_def_t *pFrame,QCameraStream * pStream);
-    bool TsMakeupProcess(mm_camera_buf_def_t *frame,QCameraStream * stream,unsigned char *makeupOutBuf,TSRect& faceRect);
+    bool TsMakeupProcess(mm_camera_buf_def_t *frame,QCameraStream * stream,TSRect& faceRect);
 #endif
 };
 
