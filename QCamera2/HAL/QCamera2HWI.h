d7fe64a6967482278042280e859b5b5b4686a819
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2015-05-06 17:00:48 -0700

    QCamera2: Perf lock for camera KPI
    Enable perf lock for different camera KPI by disabling lpm
    using setprop persist.camera.perflock.enable 1
    
    Change-Id: I0ccabde05a1bddea759a0fda4cfbba220a9e2fa1

--- QCamera2/HAL/QCamera2HWI.h
+++ QCamera2/HAL/QCamera2HWI.h
@@ -194,6 +194,22 @@ private:
     QCameraCmdThread mProcTh;
     bool             mActive;
 };
+
+class QCameraPerfLock {
+public:
+    void    lock_init();
+    void    lock_deinit();
+    int32_t lock_rel();
+    int32_t lock_acq();
+private:
+    int32_t        (*perf_lock_acq)(int, int, int[], int);
+    int32_t        (*perf_lock_rel)(int);
+    void           *dlhandle;
+    uint32_t        mPerfLockEnable;
+    pthread_mutex_t dl_mutex;
+    int32_t         mPerfLockHandle;  // Performance lock library handle
+};
+
 class QCamera2HardwareInterface : public QCameraAllocator,
         public QCameraThermalCallback, public QCameraAdjustFPS
 {
@@ -523,6 +539,7 @@ private:
     QCameraPostProcessor m_postprocessor; // post processor
     QCameraThermalAdapter &m_thermalAdapter;
     QCameraCbNotifier m_cbNotifier;
+    QCameraPerfLock m_perfLock;
     pthread_mutex_t m_lock;
     pthread_cond_t m_cond;
     api_result_list *m_apiResultList;
