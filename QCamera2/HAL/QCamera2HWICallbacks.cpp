a0112e6ac9a54ff52386608119ba38f6339c138d
Rajaram Gudivada <rajaramg@codeaurora.org>
2015-06-01 20:22:27 +0530

    QCamera2: HAL: Send Inactive AF state in landscape/sunset mode
    Issue:
    When scene mode is Landscape/Sunset, Backend AF mode is
    set to Infinity and AF state is INACTIVE and then when
    APP calls do AF, focus state is still Inactive and since
    there is no change in AF state, HAL not sending the state
    to frameworks and APP got stuck.
    
    Fix:
    Check backend AF mode is INFINITY and if its Active AF call
    then send the corresponding AF INACTIVE state to frameworks.
    
    Change-Id: I4663e29ecd3be8f257cfd18dd037c9208fb019ee

--- QCamera2/HAL/QCamera2HWICallbacks.cpp
+++ QCamera2/HAL/QCamera2HWICallbacks.cpp
@@ -1605,7 +1605,15 @@ void QCamera2HardwareInterface::metadata_stream_cb_routine(mm_camera_super_buf_t
     }
 
     IF_META_AVAILABLE(uint32_t, afState, CAM_INTF_META_AF_STATE, pMetaData) {
-        if (pme->m_currentFocusState != (*afState)) {
+        bool sendFocusUpdate=FALSE;
+        IF_META_AVAILABLE(uint32_t, afFocusMode, CAM_INTF_PARM_FOCUS_MODE, pMetaData) {
+            if (((cam_focus_mode_type)(*afFocusMode) == CAM_FOCUS_MODE_INFINITY) &&
+                    pme->mActiveAF){
+                sendFocusUpdate = TRUE;
+                CDBG("%s: send Focus Update to frameworks %d", __func__, sendFocusUpdate);
+            }
+        }
+        if (pme->m_currentFocusState != (*afState) || sendFocusUpdate) {
             pme->m_currentFocusState = (cam_af_state_t)(*afState);
             qcamera_sm_internal_evt_payload_t *payload = (qcamera_sm_internal_evt_payload_t *)
                     malloc(sizeof(qcamera_sm_internal_evt_payload_t));
