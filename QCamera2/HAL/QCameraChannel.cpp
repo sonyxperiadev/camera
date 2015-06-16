a08699a0fc7d8df3d57f2b3844f14b8a84dfecc8
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2015-06-16 11:44:40 -0700

    QCamera2: Changes to enable DSDN feature, based on h/w capability.
    Introduced new feature mask for CPP CDS. Change is to enable CPP
    CDS module if h/w supports it.
    
    Change-Id: I5f99f98fe9bfa6f58a5830329e3b9aec35458755

--- QCamera2/HAL/QCameraChannel.cpp
+++ QCamera2/HAL/QCameraChannel.cpp
@@ -1007,6 +1007,8 @@ int32_t QCameraReprocessChannel::addReprocStreamsFromSource(
                         ~CAM_QCOM_FEATURE_DENOISE2D;
                 streamInfo->reprocess_config.pp_feature_config.feature_mask &=
                         ~CAM_QCOM_FEATURE_CDS;
+                streamInfo->reprocess_config.pp_feature_config.feature_mask &=
+                        ~CAM_QCOM_FEATURE_DSDN;
 
                 if (param.isHDREnabled()
                   && !param.isHDRThumbnailProcessNeeded()){
