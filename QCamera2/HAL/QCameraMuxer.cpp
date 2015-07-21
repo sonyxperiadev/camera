ce1aed87ae9713f91705b9ca24f3c3f86b9839fc
Jingyuan Huang <jinhuang@codeaurora.org>
2015-07-21 14:23:34 -0700

    QCamera2: close devices regardless in QCameraMuxer
    Currently if we fail unbundling the devices, the devices will stay in open status.
    We need to close them regardless so that the resources can be freed up.
    
    Change-Id: I46b22c26097b4a70f1aacc93711bb33cc55cbb6c

--- QCamera2/HAL/QCameraMuxer.cpp
+++ QCamera2/HAL/QCameraMuxer.cpp
@@ -1341,7 +1341,7 @@ int QCameraMuxer::close_camera_device(hw_device_t *hw_dev)
                         rc = hwi->bundleRelatedCameras(false, sessionId);
                         if (rc != NO_ERROR) {
                             ALOGE("%s: Error Bundling physical cameras !! ", __func__);
-                            return rc;
+                            break;
                         }
                     }
                 }
@@ -1355,7 +1355,7 @@ int QCameraMuxer::close_camera_device(hw_device_t *hw_dev)
                     rc = hwi->bundleRelatedCameras(false, sessionId);
                     if (rc != NO_ERROR) {
                         ALOGE("%s: Error Bundling physical cameras !! ", __func__);
-                        return rc;
+                        break;
                     }
                 }
             }
@@ -1363,6 +1363,7 @@ int QCameraMuxer::close_camera_device(hw_device_t *hw_dev)
         cam->bSyncOn = false;
     }
 
+    // Attempt to close all cameras regardless of unbundle results
     for (uint32_t i = 0; i < cam->numCameras; i++) {
         pCam = gMuxer->getPhysicalCamera(cam, i);
         CHECK_CAMERA_ERROR(pCam);
@@ -1373,11 +1374,11 @@ int QCameraMuxer::close_camera_device(hw_device_t *hw_dev)
         rc = QCamera2HardwareInterface::close_camera_device(dev);
         if (rc != NO_ERROR) {
             ALOGE("%s: Error closing camera", __func__);
-            return rc;
         }
         pCam->hwi = NULL;
         pCam->dev = NULL;
     }
+
     // Reset JPEG client handle
     gMuxer->setJpegHandle(0);
     CDBG_HIGH("%s: X, rc: %d", __func__, rc);
