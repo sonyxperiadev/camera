ee84e2c392829a53e6f517c4c7357788d0235840
Chih-Hung Hsieh <chh@google.com>
2015-09-22 14:35:39 -0700

    Disable clang compiler to unblock angler system build.
    See bug description for the many compilation errors.
    
    BUG: 24171136
    Change-Id: I6f69d35bdda72bce0fc23fcd0191ca2f22fa2adc
    (cherry picked from commit 05f5c8ee2ecf6180e566685c282db5d004176eca)

--- QCamera2/stack/mm-camera-interface/Android.mk
+++ QCamera2/stack/mm-camera-interface/Android.mk
@@ -4,6 +4,9 @@ LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/../../../common.mk
 include $(CLEAR_VARS)
 
+# Too many clang warnings/errors, see b/23163853.
+LOCAL_CLANG := false
+
 MM_CAM_FILES := \
         src/mm_camera_interface.c \
         src/mm_camera.c \
