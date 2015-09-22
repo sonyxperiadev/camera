d0ea8ad3c803981955bfad2a3f0f982c4e4dc473
Shuzhen Wang <shuzhenw@codeaurora.org>
2015-09-21 17:53:27 -0700

    Camera3: Decrease max_buffers hint for ISP for 4K recording case
    This reduces memory footprint by reducing number of native
    buffers.
    
    Bug: 24100214
    Change-Id: I44ec8f3443e6838f1f69e8b07c34675588f4d214

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -495,7 +495,8 @@ QCamera3HardwareInterface::~QCamera3HardwareInterface()
             cam_stream_size_info_t stream_config_info;
             memset(&stream_config_info, 0, sizeof(cam_stream_size_info_t));
             stream_config_info.buffer_info.min_buffers = MIN_INFLIGHT_REQUESTS;
-            stream_config_info.buffer_info.max_buffers = MAX_INFLIGHT_REQUESTS;
+            stream_config_info.buffer_info.max_buffers =
+                    m_bIs4KVideo ? 0 : MAX_INFLIGHT_REQUESTS;
             ADD_SET_PARAM_ENTRY_TO_BATCH(mParameters, CAM_INTF_META_STREAM_INFO,
                     stream_config_info);
             int rc = mCameraHandle->ops->set_parms(mCameraHandle->camera_handle, mParameters);
@@ -2013,7 +2014,8 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
     }
 
     mStreamConfigInfo.buffer_info.min_buffers = MIN_INFLIGHT_REQUESTS;
-    mStreamConfigInfo.buffer_info.max_buffers = MAX_INFLIGHT_REQUESTS;
+    mStreamConfigInfo.buffer_info.max_buffers =
+            m_bIs4KVideo ? 0 : MAX_INFLIGHT_REQUESTS;
 
     /* Initialize mPendingRequestInfo and mPendnigBuffersMap */
     for (pendingRequestIterator i = mPendingRequestsList.begin();
@@ -3122,7 +3124,7 @@ int QCamera3HardwareInterface::processCaptureRequest(
             stream_config_info.buffer_info.min_buffers =
                     MIN_INFLIGHT_REQUESTS;
             stream_config_info.buffer_info.max_buffers =
-                    MAX_INFLIGHT_REQUESTS;
+                    m_bIs4KVideo ? 0 : MAX_INFLIGHT_REQUESTS;
             clear_metadata_buffer(mParameters);
             ADD_SET_PARAM_ENTRY_TO_BATCH(mParameters,
                     CAM_INTF_PARM_HAL_VERSION, hal_version);
