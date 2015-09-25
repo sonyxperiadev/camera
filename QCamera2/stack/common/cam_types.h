cae63a5f70aae9772b3e6c3b192d644660d1b721
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2015-09-25 15:50:29 -0700

    QCamera2: Updating resolution entry table size.
    Resolution count is more than 30 now. Change is to increase
    table size.
    
    Change-Id: I22938c3d80cda103cda6c9d46815c7ac5a2430d1

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -51,7 +51,7 @@
 #define CEILING2(X)  (((X) + 0x0001) & 0xFFFE)
 
 #define MAX_ZOOMS_CNT 91
-#define MAX_SIZES_CNT 30
+#define MAX_SIZES_CNT 40
 #define MAX_EXP_BRACKETING_LENGTH 32
 #define MAX_ROI 10
 #define MAX_STREAM_NUM_IN_BUNDLE 6
