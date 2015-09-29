8ad8b2e824a1668acb40e6bf0d51fc0b5ab824b4
Tanmaya Godbole <tgodbole@codeaurora.org>
2015-09-29 13:56:12 -0700

    Camera3: Add definitions for the custom test pattern mode
    Add the definition for TEST_PATTERN_MODE_CUSTOM1 and map it to
    the correct enum defined by the frameworks.
    CRs-fixed: 915266
    
    Change-Id: Iece33a99af4daa7279f53a39ca7cb6079b1bfdde

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -2422,6 +2422,7 @@ typedef enum {
     CAM_TEST_PATTERN_COLOR_BARS,
     CAM_TEST_PATTERN_COLOR_BARS_FADE_TO_GRAY,
     CAM_TEST_PATTERN_PN9,
+    CAM_TEST_PATTERN_CUSTOM1 = 256
 } cam_test_pattern_mode_t;
 
 typedef struct {
