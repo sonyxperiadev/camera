8e2ee86ac1d37625cf2c5bb4a70b880571539945
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2015-11-23 11:50:30 -0800

    QCamera2: Interface to communicate AWB CCM update in metadata.
    In Offline processing, ISP needs CCM update data inside
    metadata.
    
    CRs-Fixed: 952317
    Change-Id: I66ca4ef1b70b1ca68b7811b8465afdf7db0b7211

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -136,6 +136,12 @@
 /*reprocess pipeline stages are pproc and jpeg */
 #define MAX_REPROCESS_PIPELINE_STAGES 2
 
+/* Defines the number of rows in the color correction matrix (CCM) */
+#define AWB_NUM_CCM_ROWS (3)
+
+/* Defines the number of columns in the color correction matrix (CCM) */
+#define AWB_NUM_CCM_COLS (3)
+
 typedef enum {
     CAM_HAL_V1 = 1,
     CAM_HAL_V3 = 3
@@ -1421,8 +1427,17 @@ typedef struct {
 } cam_ae_exif_debug_t;
 
 typedef struct {
+    int8_t awb_ccm_enable;
+    int8_t hard_awb_ccm_flag;
+    int8_t ccm_update_flag;
+    float  ccm[AWB_NUM_CCM_ROWS][AWB_NUM_CCM_COLS];
+    float  ccm_offset[AWB_NUM_CCM_ROWS];
+} cam_awb_ccm_update_t;
+
+typedef struct {
     int32_t cct_value;
     cam_awb_gain_t rgb_gains;
+    cam_awb_ccm_update_t ccm_update;
 } cam_awb_params_t;
 
 typedef struct {
