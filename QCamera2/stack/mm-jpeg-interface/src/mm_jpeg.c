b8ae94f4ee49f2facd969d8fc7af2fdcc11796f3
Sunid Wilson <sunidw@codeaurora.org>
2016-01-19 13:16:44 -0800

    mm-jpeg-interface: Reset work buffer count to 0 after deinit
    The work buffer count is not being reset to 0 after deallocating
    the work buffers during the deinit process. Fix to reset the work
    buffer count to 0 on deinit.
    
    Change-Id: I80283e37a66d9b02bcb2729ca4b3069991a9e40c

--- QCamera2/stack/mm-jpeg-interface/src/mm_jpeg.c
+++ QCamera2/stack/mm-jpeg-interface/src/mm_jpeg.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -2083,6 +2083,7 @@ int32_t mm_jpeg_deinit(mm_jpeg_obj *my_obj)
       LOGE("Error releasing ION buffer");
     }
   }
+  my_obj->work_buf_cnt = 0;
   my_obj->jpeg_metadata = NULL;
 
   /* destroy locks */
