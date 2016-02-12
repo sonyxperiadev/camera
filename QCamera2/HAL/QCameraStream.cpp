44afd91f0ee4c2363fdcb4e9331090ecdefb762e
Susmitha Gummalla <vgummall@codeaurora.org>
2016-02-11 17:10:24 -0800

    QCamera2: Wait for deferred alloc in destructor
    In failure cases, Wait for deferred buffer allocation,
    if the BufAllocRoutine is already triggered
    
    CRs-fixed: 966229
    
    Change-Id: Ie6d32f56526ab074b611ac5b4faf4e8980ca2bf7

--- QCamera2/HAL/QCameraStream.cpp
+++ QCamera2/HAL/QCameraStream.cpp
@@ -377,6 +377,13 @@ QCameraStream::~QCameraStream()
 
     mAllocator.waitForBackgroundTask(mAllocTaskId);
     mAllocator.waitForBackgroundTask(mMapTaskId);
+    if (mBufAllocPid != 0) {
+        cond_signal(true);
+        LOGL("Wait for buf allocation thread to join");
+        // Wait for the allocation of additional stream buffers
+        pthread_join(mBufAllocPid, NULL);
+        mBufAllocPid = 0;
+    }
 
     if (mDefferedAllocation) {
         mStreamBufsAcquired = false;
@@ -2068,10 +2075,10 @@ int32_t QCameraStream::putBufs(mm_camera_map_unmap_ops_tbl_t *ops_tbl)
 
     if (mBufAllocPid != 0) {
         cond_signal(true);
-        LOGH("wait for buf allocation thread dead");
+        LOGL("wait for buf allocation thread dead");
         pthread_join(mBufAllocPid, NULL);
         mBufAllocPid = 0;
-        LOGH("return from buf allocation thread");
+        LOGL("return from buf allocation thread");
     }
 
     uint8_t numBufsToUnmap = mStreamBufs->getMappable();
