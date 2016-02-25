538a820e99f95a9dde4e3c7d5e9951d563d39577
pchilamk <pchilamk@codeaurora.org>
2016-02-24 23:57:35 -0800

    QCamera2: HAL: move cam_global_debug_level_t definition
    Move cam_global_debug_level_t definition from
    cam_types.h to mm_camera_dbh.h.
    camera_dbg.h in mm-camera2 defines its own enum
    and thus remove dependency on cam_types.h
    CRs-Fixed: 981392
    
    Change-Id: I15862ff9ec4afd58bd5ac8a19ef6b60d1d6b066a

--- QCamera2/stack/mm-camera-interface/inc/mm_camera_dbg.h
+++ QCamera2/stack/mm-camera-interface/inc/mm_camera_dbg.h
@@ -46,6 +46,18 @@ typedef enum {
     CAM_LAST_MODULE
 } cam_modules_t;
 
+/* values that persist.camera.global.debug can be set to */
+/* all camera modules need to map their internal debug levels to this range */
+typedef enum {
+    CAM_GLBL_DBG_NONE  = 0,
+    CAM_GLBL_DBG_ERR   = 1,
+    CAM_GLBL_DBG_WARN  = 2,
+    CAM_GLBL_DBG_HIGH  = 3,
+    CAM_GLBL_DBG_DEBUG = 4,
+    CAM_GLBL_DBG_LOW   = 5,
+    CAM_GLBL_DBG_INFO  = 6
+} cam_global_debug_level_t;
+
 extern int g_cam_log[CAM_LAST_MODULE][CAM_GLBL_DBG_INFO + 1];
 
 #define FATAL_IF(cond, ...) LOG_ALWAYS_FATAL_IF(cond, ## __VA_ARGS__)
