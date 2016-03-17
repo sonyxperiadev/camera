602c71e7db68ff8b36ce29cd3f55915836d3ac90
Rajaram Gudivada <rajaramg@codeaurora.org>
2016-03-17 15:00:23 +0530

    QCamera2: HAL: Avoid the redundant Steam Info IOCTL
    Issue:
    During Prepare preview, camera HAL is sending two IOCTL.s
    for just setting the configuration of streams to backend i.e,
    one with StreamInfo Zero and another IOCTL with actual
    Stream configuration.
    
    Fix:
    Send only one IOCTL(CAM_INTF_META_STREAM_INFO)
    with complete stream configuration to backend by which
    it can reset the old info and take the new configuration
    into account.
    
    CRs-Fixed: 987463
    Change-Id: I0a8151c0c9d7e411d226dd9015512827eb82a326

--- QCamera2/HAL/QCameraParameters.cpp
+++ QCamera2/HAL/QCameraParameters.cpp
@@ -12687,12 +12687,9 @@ bool QCameraParameters::setStreamConfigure(bool isCapture,
     memset(&stream_config_info, 0, sizeof(stream_config_info));
     stream_config_info.num_streams = 0;
 
-    if (m_bStreamsConfigured) {
+    if (resetConfig) {
         LOGH("Reset stream config!!");
         rc = sendStreamConfigInfo(stream_config_info);
-        m_bStreamsConfigured = false;
-    }
-    if (resetConfig) {
         LOGH("Done Resetting stream config!!");
         return rc;
     }
@@ -12929,8 +12926,6 @@ bool QCameraParameters::setStreamConfigure(bool isCapture,
     }
 
     rc = sendStreamConfigInfo(stream_config_info);
-    m_bStreamsConfigured = true;
-
     return rc;
 }
 
