ef172e74f2a1f4ba427aa059ceca108b3f1a4d6b
Ranjith Kagathi Ananda <ranjith@codeaurora.org>
2016-08-11 18:32:54 -0700

    QCamera2/HAL3: WUDL: Override timeout method in pic and yuv
    PicChannel uses heap buffers for YUV output. Override the
    timeout method to cancel_buf on the heap buffer index and
    report the BUFFER_STATUS_ERROR during jpeg callback
    
    CRs-Fixed: 991191
    
    Change-Id: I269eae0566d01c379e2022a93fcc3889d6b435fd

--- QCamera2/HAL3/QCamera3Channel.h
+++ QCamera2/HAL3/QCamera3Channel.h
@@ -70,6 +70,11 @@ typedef void (*channel_cb_routine)(mm_camera_super_buf_t *metadata,
                                 camera3_stream_buffer_t *buffer,
                                 uint32_t frame_number, bool isInputBuffer,
                                 void *userdata);
+
+typedef void (*channel_cb_buffer_err)(QCamera3Channel* ch, uint32_t frameNumber,
+                                camera3_buffer_status_t err,
+                                void *userdata);
+
 class QCamera3Channel
 {
 public:
@@ -77,6 +82,7 @@ public:
                    uint32_t channel_handle,
                    mm_camera_ops_t *cam_ops,
                    channel_cb_routine cb_routine,
+                   channel_cb_buffer_err cb_buf_err,
                    cam_padding_info_t *paddingInfo,
                    cam_feature_mask_t postprocess_mask,
                    void *userData, uint32_t numBuffers);
@@ -154,6 +160,7 @@ protected:
 
     QCamera3HeapMemory *mStreamInfoBuf;
     channel_cb_routine mChannelCB;
+    channel_cb_buffer_err mChannelCbBufErr;
     //cam_padding_info_t *mPaddingInfo;
     cam_feature_mask_t mPostProcMask;
     uint32_t mYUVDump;
@@ -180,6 +187,7 @@ public:
            uint32_t channel_handle,
            mm_camera_ops_t *cam_ops,
            channel_cb_routine cb_routine,
+           channel_cb_buffer_err cb_buffer_err,
            cam_padding_info_t *paddingInfo,
            void *userData,
            camera3_stream_t *stream,
@@ -226,7 +234,7 @@ public:
     int32_t checkStreamCbErrors(mm_camera_super_buf_t *super_frame,
             QCamera3Stream *stream);
     int32_t getStreamSize(cam_dimension_t &dim);
-    int32_t timeoutFrame(uint32_t frameNumber);
+    virtual int32_t timeoutFrame(uint32_t frameNumber);
 
     QCamera3PostProcessor m_postprocessor; // post processor
     void showDebugFPS(int32_t streamType);
@@ -276,6 +284,7 @@ public:
                     uint32_t channel_handle,
                     mm_camera_ops_t *cam_ops,
                     channel_cb_routine cb_routine,
+                    channel_cb_buffer_err cb_buffer_err,
                     cam_padding_info_t *paddingInfo,
                     void *userData,
                     camera3_stream_t *stream,
@@ -310,6 +319,7 @@ public:
                     uint32_t channel_handle,
                     mm_camera_ops_t *cam_ops,
                     channel_cb_routine cb_routine,
+                    channel_cb_buffer_err cb_buffer_err,
                     cam_padding_info_t *paddingInfo,
                     cam_feature_mask_t postprocess_mask,
                     void *userData,
@@ -342,6 +352,7 @@ public:
                     uint32_t channel_handle,
                     mm_camera_ops_t *cam_ops,
                     channel_cb_routine cb_routine,
+                    channel_cb_buffer_err cb_buffer_err,
                     cam_padding_info_t *paddingInfo,
                     void *userData,
                     camera3_stream_t *stream,
@@ -413,6 +424,7 @@ public:
             uint32_t channel_handle,
             mm_camera_ops_t *cam_ops,
             channel_cb_routine cb_routine,
+            channel_cb_buffer_err cb_buffer_err,
             cam_padding_info_t *paddingInfo,
             void *userData,
             camera3_stream_t *stream,
@@ -474,6 +486,7 @@ public:
             uint32_t channel_handle,
             mm_camera_ops_t *cam_ops,
             channel_cb_routine cb_routine,
+            channel_cb_buffer_err cb_buffer_err,
             cam_padding_info_t *paddingInfo,
             void *userData,
             camera3_stream_t *stream,
@@ -497,6 +510,7 @@ public:
     virtual QCamera3StreamMem *getStreamBufs(uint32_t le);
     virtual void putStreamBufs();
     virtual reprocess_type_t getReprocessType();
+    virtual int32_t timeoutFrame(uint32_t frameNumber);
 
     QCamera3Exif *getExifData(metadata_buffer_t *metadata,
             jpeg_settings_t *jpeg_settings);
@@ -535,6 +549,7 @@ public:
                             uint32_t channel_handle,
                             mm_camera_ops_t *cam_ops,
                             channel_cb_routine cb_routine,
+                            channel_cb_buffer_err cb_buffer_err,
                             cam_padding_info_t *paddingInfo,
                             cam_feature_mask_t postprocess_mask,
                             void *userData, void *ch_hdl);
--- QCamera2/HAL3/QCamera3HWI.h
+++ QCamera2/HAL3/QCamera3HWI.h
@@ -98,6 +98,8 @@ typedef struct {
     camera3_stream_t *stream;
     // Buffer handle
     buffer_handle_t *buffer;
+    // Buffer status
+    camera3_buffer_status_t bufStatus = CAMERA3_BUFFER_STATUS_OK;
 } PendingBufferInfo;
 
 typedef struct {
@@ -116,6 +118,7 @@ public:
     List<PendingBuffersInRequest> mPendingBuffersInRequest;
     uint32_t get_num_overall_buffers();
     void removeBuf(buffer_handle_t *buffer);
+    int32_t getBufErrStatus(buffer_handle_t *buffer);
 };
 
 
@@ -223,6 +226,10 @@ public:
     const char *getEepromVersionInfo();
     const uint32_t *getLdafCalib();
     void get3AVersion(cam_q3a_version_t &swVersion);
+    static void setBufferErrorStatus(QCamera3Channel*, uint32_t frameNumber,
+            camera3_buffer_status_t err, void *userdata);
+    void setBufferErrorStatus(QCamera3Channel*, uint32_t frameNumber,
+            camera3_buffer_status_t err);
 
     // Get dual camera related info
     bool isDeviceLinked() {return mIsDeviceLinked;}
