dc35aa2b9c6028d7347353f3d8c8f37eef0f9d65
David <Davidteri91@gmail.com>
2016-10-26 23:59:41 +0200

    [NOUGAT]camera: switch media HAL at msm8996
    see: https://android.googlesource.com/platform/hardware/qcom/media/+/android-7.1.0_r4
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- QCamera2/Android.mk
+++ QCamera2/Android.mk
@@ -82,8 +82,8 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/util \
         $(LOCAL_PATH)/HAL3 \
         hardware/libhardware/include/hardware \
-        hardware/qcom/media/libstagefrighthw \
-        hardware/qcom/media/mm-core/inc \
+        hardware/qcom/media/msm8996/libstagefrighthw \
+        hardware/qcom/media/msm8996/mm-core/inc \
         system/core/include/cutils \
         system/core/include/system \
         system/media/camera/include/system
