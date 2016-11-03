4dcd8db658460569c0cae821d419234c34bec6d8
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-11-03 15:38:00 +0100

    ifdef unused variable
    if USE_MEDIA_EXTENSIONS was not defined we had an unitialised variable
    that was breaking the build
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- QCamera2/HAL/QCameraMem.cpp
+++ QCamera2/HAL/QCameraMem.cpp
@@ -1553,7 +1553,9 @@ void QCameraVideoMemory::deallocate()
 camera_memory_t *QCameraVideoMemory::getMemory(uint32_t index,
         bool metadata) const
 {
+#ifdef USE_MEDIA_EXTENSIONS
     int i;
+#endif
     if (index >= mMetaBufCount || (!metadata && index >= mBufferCount))
         return NULL;
 
