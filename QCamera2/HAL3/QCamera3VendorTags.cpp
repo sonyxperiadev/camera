e8d20fac29715d9f05c15bacff8dbaccb83eaf88
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2016-11-18 21:33:07 +0530

    QCamera2:HAL3: Add vendor tag for edge mode strength.
    Issue:
    When edge mode is turned ON, default sharpenss value is being used.
    Though there is a metadata tag available in framework layer for
    edge strength, there is no corresponding public tag available in
    java layer.
    
    Fix:
    Add a vendor tag for setting edge mode strength. If tag is not
    present, use default value.
    
    Change-Id: I197aa9b7d295ea5402a21a3b011da4755bced1e0

--- QCamera2/HAL3/QCamera3VendorTags.cpp
+++ QCamera2/HAL3/QCamera3VendorTags.cpp
@@ -62,7 +62,8 @@ enum qcamera3_ext_tags qcamera3_ext3_section_bounds[QCAMERA3_SECTIONS_END -
         QCAMERA3_IR_END,
         QCAMERA3_AEC_CONVERGENCE_SPEED_END,
         QCAMERA3_AWB_CONVERGENCE_SPEED_END,
-        QCAMERA3_INSTANT_AEC_END
+        QCAMERA3_INSTANT_AEC_END,
+        QCAMERA3_SHARPNESS_END
 };
 
 typedef struct vendor_tag_info {
@@ -91,7 +92,8 @@ const char *qcamera3_ext_section_names[QCAMERA3_SECTIONS_END -
     "org.codeaurora.qcamera3.ir",
     "org.codeaurora.qcamera3.aec_convergence_speed",
     "org.codeaurora.qcamera3.awb_convergence_speed",
-    "org.codeaurora.qcamera3.instant_aec"
+    "org.codeaurora.qcamera3.instant_aec",
+    "org.codeaurora.qcamera3.sharpness"
 };
 
 vendor_tag_info_t qcamera3_privatedata[QCAMERA3_PRIVATEDATA_END - QCAMERA3_PRIVATEDATA_START] = {
@@ -211,6 +213,12 @@ vendor_tag_info_t
     { "instant_aec_available_modes",   TYPE_INT32 }
 };
 
+vendor_tag_info_t qcamera3_sharpness[QCAMERA3_SHARPNESS_END -
+        QCAMERA3_SHARPNESS_START] = {
+    {"strength", TYPE_INT32 },
+    {"range", TYPE_INT32 }
+};
+
 vendor_tag_info_t *qcamera3_tag_info[QCAMERA3_SECTIONS_END -
         VENDOR_SECTION] = {
     qcamera3_privatedata,
@@ -232,7 +240,8 @@ vendor_tag_info_t *qcamera3_tag_info[QCAMERA3_SECTIONS_END -
     qcamera3_ir,
     qcamera3_aec_speed,
     qcamera3_awb_speed,
-    qcamera3_instant_aec
+    qcamera3_instant_aec,
+    qcamera3_sharpness
 };
 
 uint32_t qcamera3_all_tags[] = {
@@ -309,7 +318,11 @@ uint32_t qcamera3_all_tags[] = {
 
     // QCAMERA3_INSTANT_AEC
     (uint32_t)QCAMERA3_INSTANT_AEC_MODE,
-    (uint32_t)QCAMERA3_INSTANT_AEC_AVAILABLE_MODES
+    (uint32_t)QCAMERA3_INSTANT_AEC_AVAILABLE_MODES,
+
+    //QCAMERA3_SHARPNESS
+    (uint32_t)QCAMERA3_SHARPNESS_STRENGTH,
+    (uint32_t)QCAMERA3_SHARPNESS_RANGE
 };
 
 const vendor_tag_ops_t* QCamera3VendorTags::Ops = NULL;
