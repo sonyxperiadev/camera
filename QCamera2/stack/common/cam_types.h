21dfa9cc05ddcba00ebe2dd2a33bc0c48e86a632
Brandon Schade <bschade@codeaurora.org>
2016-12-05 17:15:36 -0800

    QCamera2: Update metadata for spatial alignment compute output
    Update the metadata to contain the reference resolution used to
    compute the output shift.
    
    CRs-Fixed: 1059096
    
    Change-Id: I300a8e1164f3c2c217d09a61b9697d0cefe0432d

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -1849,10 +1849,7 @@ typedef struct {
     uint8_t                ready_status;
     uint8_t                is_output_shift_valid;
     cam_sac_output_shift_t output_shift;
-    uint8_t                is_wide_focus_roi_shift_valid;
-    cam_sac_output_shift_t wide_focus_roi_shift;
-    uint8_t                is_tele_focus_roi_shift_valid;
-    cam_sac_output_shift_t tele_focus_roi_shift;
+    cam_dimension_t        reference_res_for_output_shift;
 } cam_sac_output_info_t;
 
 
