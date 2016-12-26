383308e029267d37bd60a89f0f014c3c69aa0729
Arusha Goyal <arushag@codeaurora.org>
2016-12-26 10:47:05 +0530

    msm: Rename msmfalcon to sdm660
    Update the code name from msmfalcon to sdm660. As part of this, update
    the filename containing "falcon" and files content containing "falcon".
    
    Change-Id: I0565c612ff7936dac08b1789ab3b0f0d6b65e555

--- QCamera2/stack/mm-camera-interface/Android.mk
+++ QCamera2/stack/mm-camera-interface/Android.mk
@@ -25,11 +25,11 @@ ifeq ($(strip $(TARGET_USES_ION)),true)
     LOCAL_CFLAGS += -DUSE_ION
 endif
 
-ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 msm8916 apq8084 msm8084 msm8994 msm8992 msm8952 msm8937 msm8953 msm8996 msmcobalt msmfalcon msm8998, $(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 msm8916 apq8084 msm8084 msm8994 msm8992 msm8952 msm8937 msm8953 msm8996 msmcobalt sdm660 msm8998, $(TARGET_BOARD_PLATFORM)))
     LOCAL_CFLAGS += -DVENUS_PRESENT
 endif
 
-ifneq (,$(filter msm8996 msmcobalt msmfalcon msm8998,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8996 msmcobalt sdm660 msm8998,$(TARGET_BOARD_PLATFORM)))
     LOCAL_CFLAGS += -DUBWC_PRESENT
 endif
 
