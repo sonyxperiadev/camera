288caccd780c5128596dddd7c5969b238b42e658
Krupal Divvela <kdivvela@codeaurora.org>
2017-03-21 19:41:46 +0530

    QCamera2: HAL: Enable lib2d rotation
    Enable lib2d rotation instead of using
    Exif rotation. By default this is disabled and
    use this setprop to enable this:
    
    adb shell setprop persist.camera.lib2d.rotation on
    
    This is only for HAL1.
    
    Change-Id: I1cbd7722d34761e3a78d90c1d17f216e9255ebc2

--- QCamera2/stack/mm-jpeg-interface/Android.mk
+++ QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -12,7 +12,7 @@ LOCAL_CFLAGS += -Wall -Wextra -Werror -Wno-unused-parameter
 LOCAL_C_INCLUDES+= $(kernel_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 
-LIB2D_ROTATION=false
+LIB2D_ROTATION=true
 
 LOCAL_C_INCLUDES += \
     frameworks/native/include/media/openmax \
