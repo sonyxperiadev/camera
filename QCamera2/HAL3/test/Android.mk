6bdf44df58ce38a00eca3e2efe5799a57c8ead56
Camera Software Integration <camswint@localhost>
2017-04-10 12:08:58 -0600

    Revert "QCamera2: Enable LTO"
    This reverts commit b468b2cf622fd9b628c022d117337d082422858d.
    
    Change-Id: I567c8dcbbd9ef03fde9459ea0fa3bc7db2839cef

--- QCamera2/HAL3/test/Android.mk
+++ QCamera2/HAL3/test/Android.mk
@@ -45,8 +45,6 @@ LOCAL_SHARED_LIBRARIES:= libutils libcamera_client liblog libcamera_metadata lib
 LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
 
 LOCAL_MODULE:= hal3-test-app
-LOCAL_SDCLANG := $(SDCLANGSAVE)
-LOCAL_SDCLANG_LTO := $(SDCLANGSAVE)
 
 LOCAL_CFLAGS += -Wall -Wextra -Werror
 
