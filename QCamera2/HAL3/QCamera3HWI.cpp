49319c9019e9de7380e08f2458b3827b0a2cad11
Emilian Peev <epeev@google.com>
2017-06-29 11:58:56 -0700

    QCamera2: HAL3: Fix VTS failures
    - Per API contract in case flash is not supported Hal should return
    "-ENOSYS" as error code.
    - Hal should fail when the configured stream rotation value is invalid.
    
    Bug: 63130431
    Test: adb shell
    /data/nativetest/VtsHalCameraProviderV2_4TargetTest/VtsHalCameraProviderV2_4TargetTest
    Change-Id: Ief7049d4b726943d65a865d7e295fe6b1697b118

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -13324,6 +13324,21 @@ int QCamera3HardwareInterface::validateStreamRotations(
     for (size_t j = 0; j < streamList->num_streams; j++){
         camera3_stream_t *newStream = streamList->streams[j];
 
+        switch(newStream->rotation) {
+            case CAMERA3_STREAM_ROTATION_0:
+            case CAMERA3_STREAM_ROTATION_90:
+            case CAMERA3_STREAM_ROTATION_180:
+            case CAMERA3_STREAM_ROTATION_270:
+                //Expected values
+                break;
+            default:
+                ALOGE("%s: Error: Unsupported rotation of %d requested for stream"
+                        "type:%d and stream format:%d", __func__,
+                        newStream->rotation, newStream->stream_type,
+                        newStream->format);
+                return -EINVAL;
+        }
+
         bool isRotated = (newStream->rotation != CAMERA3_STREAM_ROTATION_0);
         bool isImplDef = (newStream->format ==
                 HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED);
