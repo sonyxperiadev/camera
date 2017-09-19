da894bd67ae9a51edff760697d8b6bc61601a002
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2017-09-19 15:09:40 +0530

    QCamera2: Set bokeh mode parameter to backend.
    Issue:
    To support zoom with RTB, ISP needs to know whether bokeh
    mode is set or not. But with the current implementation,
    there is no session parameter available to know whether
    bokeh is enabled or not. Bokeh feature mask exists, but
    its set only to preview/callback streams.
    
    Fix:
    Add new session parameter to enable/disable Bokeh mode.
    
    CRs-Fixed: 2123158
    Change-Id: I174dcd9edf18a3adf20e6aedf3317c48820ebbfd

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -2482,6 +2482,7 @@ typedef enum {
     CAM_INTF_META_DC_CAPTURE,
     /* Enable/Disable AF fine scan */
     CAM_INTF_PARM_SKIP_FINE_SCAN,
+    CAM_INTF_PARM_BOKEH_MODE,
     CAM_INTF_PARM_MAX
 } cam_intf_parm_type_t;
 
