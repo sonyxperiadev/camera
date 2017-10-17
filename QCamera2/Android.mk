ecf5c51350a1c55f247375c4db14c53b0b0243c3
AngeloGioacchino Del Regno <kholk11@gmail.com>
2017-10-17 14:46:44 +0200

    QCamera2: Use QCOM_MEDIA_ROOT for media headers
    We declare QCOM_MEDIA_ROOT in our sony-common Android.mk
    and we can use this variable to dynamically choose the HAL headers
    location for every future release, instead of changing it manually
    everytime.
    This also helps development when we need to change media HAL path..

--- QCamera2/Android.mk
+++ QCamera2/Android.mk
@@ -94,8 +94,8 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/util \
         $(LOCAL_PATH)/HAL3 \
         hardware/libhardware/include/hardware \
-        hardware/qcom/media/libstagefrighthw \
-        hardware/qcom/media/mm-core/inc \
+        $(QCOM_MEDIA_ROOT)/libstagefrighthw \
+        $(QCOM_MEDIA_ROOT)/mm-core/inc \
         system/core/include/cutils \
         system/core/include/system \
         system/media/camera/include/system \
