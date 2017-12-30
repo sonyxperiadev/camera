1010c322c37c6841f88643e764afcc79ecb6f86f
Alin Jerpelea <alin.jerpelea@sony.com>
2017-12-30 15:36:01 +0100

    fix build: add missing VANILLA_HAL
    CAMERA_MSG_META_DATA is not available in AOSP
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -7260,7 +7260,9 @@ int32_t QCamera2HardwareInterface::processRTBData(cam_rtb_msg_type_t rtbData)
         qcamera_callback_argm_t cbArg;
         memset(&cbArg, 0, sizeof(qcamera_callback_argm_t));
         cbArg.cb_type = QCAMERA_DATA_CALLBACK;
+#ifndef VANILLA_HAL
         cbArg.msg_type = CAMERA_MSG_META_DATA;
+#endif
         cbArg.data = buffer;
         cbArg.user_data = buffer;
         cbArg.cookie = this;
