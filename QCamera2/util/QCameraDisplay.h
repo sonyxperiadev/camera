0856148e6170a07766a942fe36f6d257928145e1
David <Davidteri91@gmail.com>
2018-08-11 17:48:26 +0200

    Revert "Revert "QCamera2:QCameraDisplay:Merging fix of DisplayService transaction failed.""
    This reverts commit ddbe0547eb0cf661c8ba3fabb838a05c8eebc4ab.

--- QCamera2/util/QCameraDisplay.h
+++ QCamera2/util/QCameraDisplay.h
@@ -44,8 +44,10 @@ using ::android::frameworks::displayservice::V1_0::IDisplayEventReceiver;
 using ::android::frameworks::displayservice::V1_0::IDisplayService;
 using ::android::frameworks::displayservice::V1_0::IEventCallback;
 using ::android::frameworks::displayservice::V1_0::Status;
+using ::android::hardware::hidl_death_recipient;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
+using ::android::wp;
 using ::android::sp;
 #else //USE_DISPLAY_SERVICE
 #include <utils/Timers.h>
@@ -57,11 +59,7 @@ namespace qcamera {
 #define CAMERA_NUM_VSYNC_INTERVAL_HISTORY  8
 #define NSEC_PER_MSEC 1000000LLU
 
-#ifdef USE_DISPLAY_SERVICE
-class QCameraDisplay : public IEventCallback {
-#else //USE_DISPLAY_SERVICE
 class QCameraDisplay {
-#endif //USE_DISPLAY_SERVICE
 
 public:
     QCameraDisplay();
@@ -71,18 +69,24 @@ public:
     void        init();
     bool        isInited() { return m_bInitDone; }
     bool        isSyncing() {return m_bSyncing; }
-
     bool        startVsync(bool start);
+    static QCameraDisplay* instance();
 
+class DisplayEventCallback : public IEventCallback {
     Return<void> onVsync(uint64_t timestamp, uint32_t count) override {
         ALOGV("onVsync: timestamp=%llu count=%d", timestamp, count);
-        computeAverageVsyncInterval(timestamp);
+        QCameraDisplay::instance()->computeAverageVsyncInterval(timestamp);
         return Void();
     }
     Return<void> onHotplug(uint64_t timestamp, bool connected) override {
         ALOGV("onHotplug: timestamp=%llu connected=%s", timestamp, connected ? "true" : "false");
         return Void();
     }
+};
+
+class DeathRecipient : virtual public hidl_death_recipient {
+    virtual void serviceDied(uint64_t cookie, const wp<android::hidl::base::V1_0::IBase>& who) override;
+};
 
 #else //USE_DISPLAY_SERVICE
     static int   vsyncEventReceiverCamera(int fd, int events, void* data);
@@ -113,6 +117,9 @@ private:
     bool     m_bSyncing;
     sp<IDisplayEventReceiver> mDisplayEventReceiver;
     sp<IDisplayService> mDisplayService;
+    sp<DeathRecipient> mDeathRecipient;
+    sp<DisplayEventCallback> mDisplayEventCallback;
+    static QCameraDisplay *mCameraDisplay;
 #else //USE_DISPLAY_SERVICE
     pthread_t mVsyncThreadCameraHandle;
     uint32_t  mThreadExit;
