73bf4f08517e53e011295488f6fd52cfc90fea4e
Abhinav Sohane <abhi@codeaurora.org>
2019-02-11 20:50:19 +0530

    QCamera2:HAL3: Fix for exif params(exposure metering) not updated.
    Removed copying exif params to metadata if its already valid.
    added copying 3a params in mExifParams.
    debug_params in encode_job is not used, so removing malloc
    for jpeg_job.debug params
    
    Change-Id: I9518170f488b8fb035eaa86e664c6d4a32213734
    CRs-fixed:2356005

--- QCamera2/stack/common/cam_intf.h
+++ QCamera2/stack/common/cam_intf.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -901,6 +901,9 @@ typedef struct cam_stream_info {
     } \
 }
 
+#define IS_META_VALID(TABLE_PTR, META_ID) ((NULL != TABLE_PTR) ? \
+                                             TABLE_PTR->is_valid[META_ID] : \
+                                             FALSE )
 /************************************
 * Custom parameter data definition
 *************************************/
