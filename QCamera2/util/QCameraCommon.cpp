d7dc4eddc396692d8ec5d8f7f4c1ecf103953707
Abhinav Sohane <abhi@codeaurora.org>
2019-02-20 11:23:55 +0530

    QCamera2: Enable/Disable HAL1 based on setprop.
    enabe/disbale hal1 using persist.vendor.camera.hal1.enable 1/0.
    
    Change-Id: I22d11c070b7681392f1fdbe1cb0b0038c0358b97

--- QCamera2/util/QCameraCommon.cpp
+++ QCamera2/util/QCameraCommon.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -319,6 +319,15 @@ bool QCameraCommon::isVideoUBWCEnabled()
  *==========================================================================*/
 bool QCameraCommon::needHAL1Support()
 {
+    char prop[PROPERTY_VALUE_MAX];
+    memset(prop, 0, PROPERTY_VALUE_MAX);
+    property_get("persist.vendor.camera.hal1.enable",prop,"");
+
+    if(0 < strlen(prop))
+    {
+        return (atoi(prop) == 1);
+    }
+
 #ifdef SUPPORT_ONLY_HAL3
         LOGI("ONLY HAL3 SUPPORTED");
         return FALSE;
