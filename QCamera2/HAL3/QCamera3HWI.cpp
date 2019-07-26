e593fb92babbde53b92f7ef578ede737564cbd59
Abhinav Sohane <abhi@codeaurora.org>
2019-07-26 14:47:01 +0530

    QCamera2: Enabling raw capture and dump with reprocess only requested.
    Issue: raw capture/dump is not enabled if PCR is of reprocess type.
    
    Solution: Added raw capture/dump request in PCR for reprocess type.
    
    CRs-fixed:2498745
    
    Change-Id: Ieaeef625f7f00d72be30c05513b4b33e0556ccd1

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -8029,6 +8029,7 @@ no_error:
 
     //if one o/p and one i/p i.e. reprocess request.
     uint8_t reproc_req = ((request->num_output_buffers == 1) && (request->input_buffer != NULL));
+    reproc_req = reproc_req && !mRawDumpChannel;
 
     if(!reproc_req || (internallyRequestedStreams.size()))
     {
