d3c2db78f3e4b02263bc22cce21fcd49cfea59ce
David Viteri <davidteri91@gmail.com>
2019-08-05 23:58:56 +0200

    Revert "Include what we use."
    This reverts commit c8dcff8aeed81b6accf7cef67fc9766168614878.

--- QCamera2/util/QCameraTrace.cpp
+++ QCamera2/util/QCameraTrace.cpp
@@ -28,8 +28,10 @@
 */
 
 // Camera dependencies
+#include <stdlib.h>
+#include <pthread.h>
+
 #include "QCameraTrace.h"
-#include <string.h>
 #define CAMSCOPE_MEMSTORE_SIZE 0x00100000 // 1MB
 
 volatile uint32_t kpi_camscope_flags = 0;
