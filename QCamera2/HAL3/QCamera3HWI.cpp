08edd1b4067a0ff299d13b3df86b981d2fe650f6
Abhinav Sohane <abhi@codeaurora.org>
2019-09-05 18:18:23 +0530

    QCamera2:SAT: Increased min frame wait for master switch to 2.
    Issue: Due to continous switch in each PCR, book-keeping with same
          frame_id is causing preview freeze.
    
    Solution:Tuning MIN_FRAME_WAIT_FOR_MASTER_SWITCH to 2 for avoid
          multiple switches in fast zoom-in/out.
    
    Change-Id: I7226c561f4e59956fe002706d7bee1133f91a1c2

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -4716,7 +4716,9 @@ void QCamera3HardwareInterface::handleMetadataWithLock(
             pMetaDataAux  = metadata;
         }
         resultMetadata = m_pFovControl->processResultMetadata(pMetaDataMain, pMetaDataAux);
-        if ((frame_number == UINT32_MAX) || (mHALZSL && (resultMetadata == NULL))) {
+        if ((frame_number == UINT32_MAX)
+             || (!(getHalPPType() == CAM_HAL_PP_TYPE_SAT)
+             && mHALZSL && (resultMetadata == NULL))) {
             mMetadataChannel->bufDone(metadata_buf);
             free(metadata_buf);
             return;
@@ -8405,6 +8407,16 @@ no_error:
                     mAuxParameters->is_valid[CAM_INTF_META_FRAME_NUMBER] = 0;
                 }
             }
+
+            if(IS_HAL_PP_TYPE_BOKEH)
+            {
+                uint32_t sync_param = 1;
+                ADD_SET_PARAM_ENTRY_TO_BATCH(mParameters,
+                                    CAM_INTF_PARM_SYNC_DC_PARAMETERS, sync_param);
+                ADD_SET_PARAM_ENTRY_TO_BATCH(mAuxParameters,
+                                    CAM_INTF_PARM_SYNC_DC_PARAMETERS, sync_param);
+            }
+
             if(!IS_MULTI_CAMERA)
             {
                 rc = mCameraHandle->ops->set_parms(
@@ -8412,7 +8424,6 @@ no_error:
                 if (rc < 0) {
                     LOGE("set_parms failed");
                 }
-
                 if (isDualCamera() && !IS_PP_TYPE_NONE) {
                     rc = mCameraHandle->ops->set_parms(
                           get_aux_camera_handle(mCameraHandle->camera_handle), mAuxParameters);
