b6dd22c34a945cdd765e80d5ad27e881750be169
Abhinav Sohane <abhi@codeaurora.org>
2019-09-21 00:16:37 +0530

    QCamera2:HEIF: Added HEIF on callback stream.
    Issue: SOF freeze while taking liveshot in heif usecase.
    
    Solution: Since CPP cannot handle multiple video streams with EIS
          and HEIF enabled stream is not required to be processed by
          EIS so using callback stream to process HEIF buffer request.
    Change-Id: I99a38ce7cbc3ea1da44b3712b1138d7d8f095e07

--- QCamera2/HAL3/QCamera3Channel.cpp
+++ QCamera2/HAL3/QCamera3Channel.cpp
@@ -754,7 +754,17 @@ cam_format_t QCamera3Channel::getStreamDefaultFormat(cam_stream_type_t type,
     case CAM_STREAM_TYPE_CALLBACK:
         /* Changed to macro to ensure format sent to gralloc for callback
         is also changed if the preview format is changed at camera HAL */
+#if VENUS_PRESENT
+        if(IS_USAGE_HEIF(usage))
+        {
+            streamFormat = CAM_FORMAT_YUV_420_NV12_VENUS;
+        }else{
+            streamFormat = CALLBACK_STREAM_FORMAT;
+        }
+#else
         streamFormat = CALLBACK_STREAM_FORMAT;
+#endif
+
         break;
     case CAM_STREAM_TYPE_RAW:
         streamFormat = hal_obj->mRdiModeFmt;
@@ -1712,7 +1722,11 @@ int32_t QCamera3ProcessingChannel::translateStreamTypeAndFormat(camera3_stream_t
             }
             break;
         case HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED:
-            if (stream->usage & GRALLOC_USAGE_HW_VIDEO_ENCODER) {
+            if(IS_USAGE_HEIF(stream->usage)) {
+                streamType = CAM_STREAM_TYPE_CALLBACK;
+                streamFormat = getStreamDefaultFormat(CAM_STREAM_TYPE_CALLBACK,
+                        stream->width, stream->height, stream->usage);
+            }else if (stream->usage & GRALLOC_USAGE_HW_VIDEO_ENCODER) {
                 streamType = CAM_STREAM_TYPE_VIDEO;
                 streamFormat = getStreamDefaultFormat(CAM_STREAM_TYPE_VIDEO,
                         stream->width, stream->height, stream->usage);
@@ -1858,7 +1872,8 @@ int32_t QCamera3ProcessingChannel::setReprocConfig(reprocess_config_t &reproc_cf
         case CAM_STREAM_TYPE_CALLBACK:
         default:
             rc = mm_stream_calc_offset_snapshot(streamFormat, &reproc_cfg.input_stream_dim,
-                    reproc_cfg.padding, &reproc_cfg.input_stream_plane_info);
+                    reproc_cfg.stream_type, reproc_cfg.padding,
+                    &reproc_cfg.input_stream_plane_info);
             break;
     }
     if (rc != 0) {
@@ -3765,7 +3780,7 @@ int32_t QCamera3YUVChannel::initialize(cam_is_type_t isType)
 
     mIsType  = isType;
     mStreamFormat = getStreamDefaultFormat(CAM_STREAM_TYPE_CALLBACK,
-            mCamera3Stream->width, mCamera3Stream->height);
+            mCamera3Stream->width, mCamera3Stream->height, mCamera3Stream->usage);
 
     streamDim.width = mCamera3Stream->width;
     streamDim.height = mCamera3Stream->height;
@@ -3809,8 +3824,8 @@ int32_t QCamera3YUVChannel::initialize(cam_is_type_t isType)
     paddingInfo.width_padding = MAX(paddingInfo.width_padding, paddingInfo.height_padding);
     paddingInfo.height_padding = paddingInfo.width_padding;
 
-    rc = mm_stream_calc_offset_snapshot(mStreamFormat, &streamDim, &paddingInfo,
-            &buf_planes);
+    rc = mm_stream_calc_offset_snapshot(mStreamFormat, &streamDim,
+                 CAM_STREAM_TYPE_CALLBACK, &paddingInfo, &buf_planes);
     if (rc < 0) {
         LOGE("mm_stream_calc_offset_preview failed");
         return rc;
