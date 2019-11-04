7eb6316c674ab4fa613c5f04c61d39e664b17ceb
Abhinav Sohane <abhi@codeaurora.org>
2019-11-04 15:24:15 +0530

    QCamera2: CTS fix for testCameraCaptureResultAllKeys.
    Issue: CTS failured due unexpected ae mode received.
    
    Solution:Reseting fwAeMode value to default ANDROID_CONTROL_AE_MODE_ON.
    
    Change-Id: Id57ae1feb37b1568190c33b877c96745f257d7b9
    CRs-fixed:2558325

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -2216,6 +2216,7 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
     {
          mSavedParams.clear();
     }
+    m_fwAeMode = ANDROID_CONTROL_AE_MODE_ON;
 
     /* cache fw stream configuration, for internally reconfigure streams and then config "back" */
     cacheFwConfiguredStreams(streamList);
@@ -8229,6 +8230,9 @@ no_error:
 
     if (meta.exists(ANDROID_CONTROL_AE_MODE)) {
           pendingRequest.fwkAeMode = (uint8_t)meta.find(ANDROID_CONTROL_AE_MODE).data.u8[0];
+          m_fwAeMode = pendingRequest.fwkAeMode;
+    }else {
+         pendingRequest.fwkAeMode = m_fwAeMode;
     }
 
     //extract CAC info
