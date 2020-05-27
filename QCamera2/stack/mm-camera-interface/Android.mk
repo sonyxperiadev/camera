sonyxperiadev/camera

commit 5e5918a26696ee6730a7a4b9aefb34e6aae916fc

Remove double msm8916 definition
Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

QCamera2/stack/mm-camera-interface/Android.mk
@@ -27,7 +27,7 @@ ifeq ($(shell expr $(TARGET_KERNEL_VERSION) \>= 4.4), 1)
 LOCAL_CFLAGS += -DUSE_KERNEL_VERSION_GE_4_4_DEFS
 endif
 
-ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 msm8916 apq8084 msm8084 msm8994 msm8992 msm8952 msm8937 msm8953 msm8996 sdm660 msm8998 apq8098_latv $(TRINKET), $(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 apq8084 msm8084 msm8994 msm8992 msm8952 msm8937 msm8953 msm8996 sdm660 msm8998 apq8098_latv, $(TARGET_BOARD_PLATFORM)))
     LOCAL_CFLAGS += -DVENUS_PRESENT
 endif
 

Android.mk
@@ -1,6 +1,5 @@
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8916
 MM_V4L2_DRIVER_LIST += msm8226
 MM_V4L2_DRIVER_LIST += msm8610
 MM_V4L2_DRIVER_LIST += apq8084

commit 59274c8fdd88c04be48ccac64c2b9a5cff53a3f2

camera : fix TRINKET definitions
commit 5e5918a26696ee6730a7a4b9aefb34e6aae916fc
accidentally removed TRINKET config

Change-Id: Ic573a8285a586d365fa9a4cc930d52df56e87578

QCamera2/stack/mm-camera-interface/Android.mk
@@ -27,7 +27,7 @@ ifeq ($(shell expr $(TARGET_KERNEL_VERSION) \>= 4.4), 1)
 LOCAL_CFLAGS += -DUSE_KERNEL_VERSION_GE_4_4_DEFS
 endif
 
-ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 apq8084 msm8084 msm8994 msm8992 msm8952 msm8937 msm8953 msm8996 sdm660 msm8998 apq8098_latv, $(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8974 msm8916 msm8226 msm8610 apq8084 msm8084 msm8994 msm8992 msm8952 msm8937 msm8953 msm8996 sdm660 msm8998 apq8098_latv $(TRINKET), $(TARGET_BOARD_PLATFORM)))
     LOCAL_CFLAGS += -DVENUS_PRESENT
 endif
 